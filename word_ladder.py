7a5541b14155eb22b1065001fc3da610f3158ffd
Vikramaditya Jhunjhunwala <vjhunjhunwala20@cmc.edu>
2020-02-22 20:46:08 -0800

    First try

--- word_ladder.py
+++ word_ladder.py
@@ -2,14 +2,31 @@
 
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
+    with open (words5.dict) as wordList:
+        xs = wordList.readlines()
+    print ('xs=', xs)
+
+    wordList.append(endWord)
+    queue = collections.deque([[beginWord, 1]])
+    leng = len(beginWord)
+    
+    while queue:
+        word, length = queue.popleft()
+        if word == endWord:
+            return length
+        for n in wordList:
+            if sum(n[i]!=word[i] for i in xrange(leng))==1:
+                wordList.remove(n)
+                queue.append([n,length+1])
+    return 0
+
+
     '''
     Returns a list satisfying the following properties:
-
     1. the first element is `start_word`
     2. the last element is `end_word`
     3. elements at index i and i+1 are `_adjacent`
     4. all elements are entries in the `dictionary_file` file
-
     For example, running the command
     ```
     word_ladder('stone','money')
@@ -24,7 +41,6 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     ['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey', 'honey', 'money']
     ```
     (We cannot use doctests here because the outputs are not unique.)
-
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
@@ -41,7 +57,6 @@ def _adjacent(word1, word2):
     '''
     Returns True if the input words differ by only a single character;
     returns False otherwise.
-
     >>> _adjacent('phone','phony')
     True
     >>> _adjacent('stone','money')
